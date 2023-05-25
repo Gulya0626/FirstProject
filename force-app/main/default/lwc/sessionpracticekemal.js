ff3d7f52682488ee1addffcf42a9db1b49ec6161
Gulya <Gulyakg@yahoo.com>
2023-05-25 11:11:32 -0500

    lwc practices

---
+++ force-app/main/default/lwc/sessionpracticekemal.js
@@ -0,0 +1,23 @@
+// let num1 = 10;
+// let num2 = 20;
+
+// let name = 'Osman';
+
+// console.log(typeof 10);
+// console.log(typeof 'Osman');
+
+// console.log(num1+num2);
+// console.log(num1>num2 && num2>num1);
+
+// let addNumbers = (num1 + num2) => {
+//     return num1+num2;
+// }
+
+// let addNumbers1 = (num1, num2) => num1+num2;
+// console.log(addNumber1(num1, num2));
+
+// function addNumbers2(num1, num2){
+//     return num1+num2;
+// }
+
+// console.log(addNumbers(num1+num2)); 
